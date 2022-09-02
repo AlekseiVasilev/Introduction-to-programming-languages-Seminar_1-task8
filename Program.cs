13da211ebc2623412c38a1c73e21afcbab32f948
Aleksei <vasilliev@gmail.com>
2022-09-03 01:10:28 +0300

    initiation commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = 0;
+
+while (number1 > number2)
+    {
+    number2 = number2 + 2;
+    if (number1 > number2)
+    Console.Write($"{number2} ");
+    }
